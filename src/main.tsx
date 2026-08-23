1b1d58b3ee26aa5592e3ce73d0f773846e714eed
ishak595-code <ishak595@gmail.com>
2026-08-24 00:51:58 +0200

    Load connected product discovery styling

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import './features/customer-experience/videoRecordingExact.css';
 import './features/customer-experience/homeMerchandisingUpgrade.css';
 import './features/customer-experience/homeOneRowPremium.css';
 import './features/customer-experience/storefrontNarrativePremium.css';
+import './features/customer-experience/productDiscoveryPremium.css';
 import { initNativeFeatures } from './native';
 import { initNativePushListeners } from './features/notifications/nativePush';
 import { applyThemeToDocument, resolveInitialTheme } from './features/appearance/theme';
